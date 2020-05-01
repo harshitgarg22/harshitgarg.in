1e9ac6529920ff49328525d5bb2121387f6df45b
harshitgarg22 <hg1229@gmail.com>
2020-05-01 21:52:43 +0530

    Add support for greetings

--- cgi-bin/app.py
+++ cgi-bin/app.py
@@ -5,4 +5,25 @@ app = Flask(__name__, static_folder="./assets", static_url_path="/static")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    greetings = generateGreetings()
+
+    return render_template("index.html", greetings=greetings)
+
+
+def generateGreetings():
+    greetings = {}
+
+    DISPLAY_TEXT = '"Hello World!"'
+    greetings["languages"] = ["JavaScript", "C++", "C", "python", "bash"]
+    # greetings["headers"] = ["", "#include <iostream>", "#include <stdio.h>", "", ""]
+    greetings["display_text"] = DISPLAY_TEXT
+    greetings["commands_prefix"] = [
+        "console.log(",
+        "cout<<",
+        "printf(",
+        "print(",
+        "echo ",
+    ]
+    greetings["commands_suffix"] = [");", ";", ");", ")", ""]
+
+    return greetings
