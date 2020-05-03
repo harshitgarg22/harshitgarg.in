508be810a318a5b819a0077ab7189127d9065366
harshitgarg22 <hg1229@gmail.com>
2020-05-04 01:07:40 +0530

    json dump film

---
+++ daily_jobs/jsonDumpFilm.py
@@ -0,0 +1,12 @@
+from stripFilms import stripFilms
+import feedparser as fp
+import json
+import os
+
+DATA_LOCATION = os.path.join("cgi-bin", "assets", "data")
+
+jsonOutput = fp.parse(os.path.join(DATA_LOCATION, "letterboxd_rss.xml"), "r")
+films = stripFilms(jsonOutput)
+
+with open(os.path.join(DATA_LOCATION, "filmData.json"), "w") as filmData:
+    filmData.write(json.dumps(films))
