951b65bcb289faa91d79e3760dc641d2381f1a5e
harshitgarg22 <hg1229@gmail.com>
2020-05-04 16:49:49 +0530

    Films (99%) complete!

--- daily_jobs/stripFilms.py
+++ daily_jobs/stripFilms.py
@@ -13,6 +13,10 @@ def stripFilms(filmRawData):
     open('./error.json', 'w').close()
     for eachFilm in filmRawData["entries"]:
         try:
+            # A really complex parsing with no documentation and bad variable name muhahahahahahaha
+            soup = (''.join(str(y) for a in [x.contents for x in BSHTML(eachFilm.get("summary"), 'html.parser').find_all('p')[1:]] for y in a)).replace('<br/>', '\n\n')
+            imgsrc = BSHTML(eachFilm.get("summary"), 'html.parser').find('img')['src']
+            
             films.append({
             "title": eachFilm.get("letterboxd_filmtitle"),
             "year": eachFilm.get("letterboxd_filmyear"), 
@@ -20,13 +24,12 @@ def stripFilms(filmRawData):
             "watcheddate": eachFilm.get("letterboxd_watcheddate"),
             "rewatch": eachFilm.get("letterboxd_rewatch"),
             "rating": eachFilm.get("letterboxd_memberrating"),
-            # "published": eachFilm.get("published"),
-            # "imgsrc": imgsrc,
-            # "description": text,
-            # "author": eachFilm["author"]
+            "summary": str(soup),
+            "imgsrc": imgsrc,
             })
         except:
             with open('./error.json', 'a') as f:
-                f.write(json.dumps(eachFilm) + '\n')
+                e = sys.exc_info()
+                f.write(str(e) + json.dumps(eachFilm) + '\n')
 
     return films
