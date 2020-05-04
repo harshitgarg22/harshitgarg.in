3adb4d4cda900c8915cd00f2740a9dfeeca2e97c
harshitgarg22 <hg1229@gmail.com>
2020-05-04 23:24:27 +0530

    Update stripFilms for HTML tags

--- daily_jobs/stripFilms.py
+++ daily_jobs/stripFilms.py
@@ -1,9 +1,54 @@
 import json
 import sys
-from bs4 import BeautifulSoup as BSHTML
+from bs4 import BeautifulSoup as BSHTML, Comment
+import re
+import subprocess
+import os
+
+def sanitize_html(html):
+    """
+        Returns sanitize html to protect against XSS
+    """
+    # God bless https://chase-seibert.github.io/blog/2011/01/28/sanitize-html-with-beautiful-soup.html
+
+    if not html:
+        return None
+
+    blacklist = ["script", "style", "img"]
+
+    soup = BSHTML(html, 'html.parser')        
+    # now strip HTML we don't like.
+    for tag in soup.findAll():
+        if tag.name.lower() in blacklist:
+            # blacklisted tags are removed in their entirety
+            tag.extract()
+        else:
+            tag.attrs = []
+
+    # scripts can be executed from comments in some cases
+    comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+    for comment in comments:
+        comment.extract()
+
+    safe_html = soup
+
+    if safe_html == ", -":
+        return None
+
+    return safe_html
+
+
+def _attr_name_whitelisted(attr_name):
+    return attr_name.lower() in ["href", "style", "color", "size", "bgcolor", "border", ""]
+
+def safe_css(attr, css):
+    if attr == "style":
+        return re.sub("(width|height):[^;]+;", "", css)
+    return css
 
 def stripFilms(filmRawData):
-    """Returns a files list containing stripped-down output from a letterboxd json file
+    """
+        Returns a files list containing stripped-down output from a letterboxd json file
     
         Keyword Arguments:
         filmRawData: Raw JSON letterboxd data.
@@ -12,9 +57,23 @@ def stripFilms(filmRawData):
     films = []
     open('./error.json', 'w').close()
     for eachFilm in filmRawData["entries"]:
+        safe_html = sanitize_html(eachFilm.get("summary"))
         try:
             # A really complex parsing with no documentation and bad variable name muhahahahahahaha
-            soup = (''.join(str(y) for a in [x.contents for x in BSHTML(eachFilm.get("summary"), 'html.parser').find_all('p')[1:]] for y in a)).replace('<br/>', '\n\n')
+            # soup = (''.join(str(z) for a in [x for x in safe_html.find_all('p')] for z in a))
+            # soup = [y for x in safe_html.find_all('p')[1:] for y in x.contents]
+            # soup = []
+            review = (''.join(str(y) for a in [x.contents for x in safe_html.find_all('p')[1:]] for y in a)).replace('<br/>', '\n\n')
+
+            # for x in safe_html.find_all('p')[1:]:
+            #     for y in x.contents:
+            #         soup.append(y)
+
+            # review = ''
+            # for x in soup:
+            #     review = review + (str(x))
+
+
             imgsrc = BSHTML(eachFilm.get("summary"), 'html.parser').find('img')['src']
             
             films.append({
@@ -24,7 +83,7 @@ def stripFilms(filmRawData):
             "watcheddate": eachFilm.get("letterboxd_watcheddate"),
             "rewatch": eachFilm.get("letterboxd_rewatch"),
             "rating": eachFilm.get("letterboxd_memberrating"),
-            "summary": str(soup),
+            "summary": review,
             "imgsrc": imgsrc,
             })
         except:
