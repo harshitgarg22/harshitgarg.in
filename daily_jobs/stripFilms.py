f73eb1c6ffa2d3b96db3cb943ed424afe0c03e30
harshitgarg22 <hg1229@gmail.com>
2020-05-07 02:22:08 +0530

    We don't need no, sanitation.

--- daily_jobs/stripFilms.py
+++ daily_jobs/stripFilms.py
@@ -5,47 +5,6 @@ import re
 import subprocess
 import os
 
-def sanitize_html(html):
-    """
-        Returns sanitize html to protect against XSS
-    """
-    # God bless https://chase-seibert.github.io/blog/2011/01/28/sanitize-html-with-beautiful-soup.html
-
-    if not html:
-        return None
-
-    blacklist = ["script", "style", "img"]
-
-    soup = BSHTML(html, 'html.parser')        
-    # now strip HTML we don't like.
-    for tag in soup.findAll():
-        if tag.name.lower() in blacklist:
-            # blacklisted tags are removed in their entirety
-            tag.extract()
-        else:
-            tag.attrs = []
-
-    # scripts can be executed from comments in some cases
-    comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-    for comment in comments:
-        comment.extract()
-
-    safe_html = soup
-
-    if safe_html == ", -":
-        return None
-
-    return safe_html
-
-
-def _attr_name_whitelisted(attr_name):
-    return attr_name.lower() in ["href", "style", "color", "size", "bgcolor", "border", ""]
-
-def safe_css(attr, css):
-    if attr == "style":
-        return re.sub("(width|height):[^;]+;", "", css)
-    return css
-
 def stripFilms(filmRawData):
     """
         Returns a files list containing stripped-down output from a letterboxd json file
@@ -61,10 +20,10 @@ def stripFilms(filmRawData):
         try:
             # A really complex parsing with no documentation and bad variable name muhahahahahahaha
             # soup = (''.join(str(z) for a in [x for x in safe_html.find_all('p')] for z in a))
-            soup = [y for x in safe_html.find_all('p')[1:] for y in x.contents]
-            # soup = []
+            soup = [str(y) for x in safe_html.find_all('p')[1:] for y in x.contents]
+            # soup = [str(x.contents) for x in safe_html.find_all('p')[1:]]
             # review = (''.join(str(y) for a in [x.contents for x in safe_html.find_all('p')[1:]] for y in a)).replace('<br/>', '\n\n')
-            review = str(soup)
+            review = (soup)
             # for x in safe_html.find_all('p')[1:]:
             #     for y in x.contents:
             #         soup.append(y)
