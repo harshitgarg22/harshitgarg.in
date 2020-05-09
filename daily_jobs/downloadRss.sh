c725f92d3fb4810bb068ab92ff1391e1c608158c
Harshit Garg <hg1229@gmail.com>
2020-05-09 06:57:11 -0700

    Update daily jobs script for working on server

--- daily_jobs/downloadRss.sh
+++ daily_jobs/downloadRss.sh
@@ -1,4 +1,5 @@
-curl https://letterboxd.com/harshitgarg/rss/ -o ./cgi-bin/assets/data/letterboxd_rss.xml
-curl https://www.goodreads.com/review/list_rss/55080672?key=XIW0JjwFXykFcPT-nxG_Qf-nhtzXR6JzVXl7eqk9PJ4w_8iQ&shelf=currently-reading -o ./cgi-bin/assets/data/goodreads_rss.xml
+curl 'https://letterboxd.com/harshitgarg/rss/' -o ../cgi-bin/assets/data/letterboxd_rss.xml
+curl 'https://www.goodreads.com/review/list_rss/55080672?key=XIW0JjwFXykFcPT-nxG_Qf-nhtzXR6JzVXl7eqk9PJ4w_8iQ&shelf=currently-reading' -o ../cgi-bin/assets/data/goodreads_rss.xml
 
-python ./daily_jobs/jsonDumpFilm.py
+source ../cgi-bin/.venv/bin/activate
+python3 ./jsonDumpFilm.py
