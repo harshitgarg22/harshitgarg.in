62dde2c154dccf308fec08b0004679ae198642eb
Harshit Garg <hg1229@gmail.com>
2020-05-09 06:57:11 -0700

    Update daily jobs script for working on server

--- daily_jobs/jsonDumpFilm.py
+++ daily_jobs/jsonDumpFilm.py
@@ -3,7 +3,7 @@ import feedparser as fp
 import json
 import os
 
-DATA_LOCATION = os.path.join("cgi-bin", "assets", "data")
+DATA_LOCATION = os.path.join("..", "cgi-bin", "assets", "data")
 
 jsonOutput = fp.parse(os.path.join(DATA_LOCATION, "letterboxd_rss.xml"), "r")
 films = stripFilms(jsonOutput)
