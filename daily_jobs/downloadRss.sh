8a040c693cdeea70305f8009079e074ef4670a6f
Harshit Garg <hg1229@gmail.com>
2020-05-16 04:46:57 -0700

    Update daily_jobs to work with cronjob

--- daily_jobs/downloadRss.sh
+++ daily_jobs/downloadRss.sh
@@ -1,5 +1,5 @@
-curl 'https://letterboxd.com/harshitgarg/rss/' -o ../cgi-bin/assets/data/letterboxd_rss.xml
-curl 'https://www.goodreads.com/review/list_rss/55080672?key=XIW0JjwFXykFcPT-nxG_Qf-nhtzXR6JzVXl7eqk9PJ4w_8iQ&shelf=currently-reading' -o ../cgi-bin/assets/data/goodreads_rss.xml
+curl 'https://letterboxd.com/harshitgarg/rss/' -o ~/harshitgarg.in/cgi-bin/assets/data/letterboxd_rss.xml
+curl 'https://www.goodreads.com/review/list_rss/55080672?key=XIW0JjwFXykFcPT-nxG_Qf-nhtzXR6JzVXl7eqk9PJ4w_8iQ&shelf=currently-reading' -o ~/harshitgarg.in/cgi-bin/assets/data/goodreads_rss.xml
 
-source ../cgi-bin/.venv/bin/activate
-python3 ./jsonDumpFilm.py
+source ~/harshitgarg.in/cgi-bin/.venv/bin/activate
+python3 ~/harshitgarg.in/daily_jobs/jsonDumpFilm.py
